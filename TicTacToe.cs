bbdcb6fac88908563614f21d1b6f85b50fcbc385
Otar <86528840+Blazkowich@users.noreply.github.com>
2023-06-12 22:21:33 +0400

    Update TicTacToe.cs

--- TicTacToe.cs
+++ TicTacToe.cs
@@ -14,12 +14,32 @@ class Program
         do
         {
             Console.WriteLine("Please Enter Number Between 1 - 9:");
+            Console.WriteLine();
             BoardViewer.DisplayBoard(game.board);
-            int inp = int.Parse(Console.ReadLine());
-            game.Replace(inp);
-            c++;
-        }
 
+            try
+            {
+                int inp = int.Parse(Console.ReadLine());
+                game.Replace(inp);
+                c++;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid Input. Please enter a Integer");
+            }
+
+            if (game.CheckWinner())
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Game Won : {(game.currentPlayer == 'X' ? 'O': 'X')}");
+            }
+
+            if (c == 9 && game.CheckWinner() == false)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Draw!");
+            }   
+        }
         while (c < 10);
     }
 }
